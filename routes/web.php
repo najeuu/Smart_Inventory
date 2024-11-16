15214894800883db16d0ac90df05b0ecccf20dd0
Nazhwa <nazhwarahmaputri@gmail.com>
2024-11-16 17:41:17 +0700

    routes update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PeminjamanController;
+use App\Http\Controllers\PengembalianController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Peminjaman Routes
+Route::get('/peminjaman', [PeminjamanController::class, 'index'])->name('peminjaman');
+
+// Pengembalian Routes 
+    Route::get('/pengembalian', [PengembalianController::class, 'index'])->name('pengembalian');
+    
